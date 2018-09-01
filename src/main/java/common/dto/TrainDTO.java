fe091b543b86111d3fc45f3698f1436418b5258c
Dmitrii Glushkov <dmitrii.glushkov@t-systems.com>
2018-09-01 22:43:37 +0300

    - fix DTO -> dto package rename

---
+++ src/main/java/common/dto/TrainDTO.java
@@ -0,0 +1,14 @@
+package common.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+
+public class TrainDTO {
+
+    @Setter @Getter
+    int capacity;
+
+    @Setter @Getter
+    String name;
+}
