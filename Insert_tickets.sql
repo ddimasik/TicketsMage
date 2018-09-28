a905d1725ea5a63927831b04ef10e6e2d1eeeaef
Dmitrii Glushkov <dmitrii.glushkov@t-systems.com>
2018-09-28 17:55:48 +0300

    - add data for demo

---
+++ Insert_tickets.sql
@@ -0,0 +1,5 @@
+INSERT INTO mysql.tickets (id, endStationId, passengerId, startStationId, trainId) VALUES (61, 5, 30, 2, 2);
+INSERT INTO mysql.tickets (id, endStationId, passengerId, startStationId, trainId) VALUES (62, 8, 31, 3, 2);
+INSERT INTO mysql.tickets (id, endStationId, passengerId, startStationId, trainId) VALUES (63, 8, 32, 2, 2);
+INSERT INTO mysql.tickets (id, endStationId, passengerId, startStationId, trainId) VALUES (64, 8, 0, 2, 2);
+INSERT INTO mysql.tickets (id, endStationId, passengerId, startStationId, trainId) VALUES (65, 8, 0, 2, 2);
